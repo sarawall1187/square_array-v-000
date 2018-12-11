5ca70cc53ce105027fc7808acc09b24567df36ba
Sara Walker <sarawall1187@gmail.com>
2018-12-11 19:52:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  array.each do |squared_number|
+   squared_number = [] << (array[0]) ** (array[0])
+  end
 end
+
+# build method
+# each number in array will be squared (times itself)
+
