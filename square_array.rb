220803214167702e055b10b6860f8076e13cd533
Sara Walker <sarawall1187@gmail.com>
2018-12-11 19:58:36 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
+# def square_array(array)
+#     array.each_with_object([]) do |number, new_array|
+#     new_array << number ** 2 
+#   end
+# end
+
 def square_array(array)
-  # your code here
+  new_array = []  
+  array.each { |number| new_array << number ** 2 } 
+  new_array 
 end
+
+# build method
+# each number in array will be squared (times itself)
+
